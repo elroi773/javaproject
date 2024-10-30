6c2fbba749e10804efd4850863555e566d7848f7
김이레 <165885725+elroi773@users.noreply.github.com>
2024-10-30 10:16:17 +0900

    수정

--- User.java
+++ User.java
@@ -3,12 +3,8 @@ package user;
 public class User {
 	private String userID;
 	private String userPassword;
-	private String userName;
-	private String userGender;
-	private String userEmaill;
-	private String userschool;
-
-	
+	private String userSchol;
+	private String userEmail;
 	
 	public String getUserID() {
 		return userID;
@@ -22,30 +18,18 @@ public class User {
 	public void setUserPassword(String userPassword) {
 		this.userPassword = userPassword;
 	}
-	public String getUserName() {
-		return userName;
+	public String getUserSchol() {
+		return userSchol;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setUserSchol(String userSchol) {
+		this.userSchol = userSchol;
 	}
-	public String getUserGender() {
-		return userGender;
+	public String getUserEmail() {
+		return userEmail;
 	}
-	public void setUserGender(String userGender) {
-		this.userGender = userGender;
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
 	}
-	public String getUserEmaill() {
-		return userEmaill;
-	}
-	public void setUserEmaill(String userEmaill) {
-		this.userEmaill = userEmaill;
-	}
-	public String getUserschool() {
-		return userschool;
-	}
-	public void setUserschool(String userschool) {
-		this.userschool = userschool;
-	}
-	
+
 	
 }
