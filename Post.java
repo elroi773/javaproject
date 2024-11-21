81f43136ca9f9c7cbc366e8a46b549db53bf290e
김이레 <165885725+elroi773@users.noreply.github.com>
2024-11-22 00:12:45 +0900

    글 쓰는 자바 파일

---
+++ Post.java
@@ -0,0 +1,34 @@
+package post;
+
+import java.util.Date;
+
+public class Post {
+    private String userID;
+    private String content;
+    private Date createdAt;
+
+    // Getters and Setters
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
